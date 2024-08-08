ab06b394346fff5557b5a4093d72639c7830366c
Ben <bc@bencarterconsulting.com>
2024-08-07 21:07:07 -0400

    Assignment completed.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+    return element + accumulator;
+  }, 0);
+
+  
+
+// let totalBatteries = 0
+// function batteries(){
+//     for (const battery of batteryBatches) {
+//         totalBatteries = batteryBatches.reduce(function (accumulator, element)){
+//             return element + accumulator
+//         }
+//     }
+//     return totalBatteries
+// }
